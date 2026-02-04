a84a783bf4ee60eeb7d48f44bc748d690b3fe073
talmo777 <76654788+talmo777@users.noreply.github.com>
2026-02-05 03:50:41 +0900

    Update types.ts

--- types.ts
+++ types.ts
@@ -93,3 +93,19 @@ export interface InternalFeedback {
   createdAt: string;
   isResolved: boolean;
 }
+
+// --- UI Constants ---
+
+export const TARGET_OPTIONS: TargetCollege[] = [
+  '공학대학',
+  '소프트웨어융합대학',
+  '첨단융합대학',
+  '약학대학',
+  '글로벌문화통상대학',
+  '커뮤니케이션&컬쳐대학',
+  '경상대학',
+  '디자인대학',
+  '예체능대학',
+  'LIONS칼리지',
+  '교수학습센터',
+];
